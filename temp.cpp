4e48a94853538278f22c1ddc7b9a25bcba3e94c2
mmizak <01169211@pw.edu.pl>
2022-05-31 18:00:53 +0200

    improved player choice in temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -10,34 +10,57 @@ using std::cin;
 int main()
 {
     Game game;
-    std::map<std::string, int> m = {{"A", 0}, {"B", 1}, {"C", 2}, {"D", 3}, {"E", 4}, {"F", 5}, {"G", 6}, {"H", 7}};
+    std::map<char, int> m = {{'A', 0}, {'B', 1}, {'C', 2}, {'D', 3}, {'E', 4}, {'F', 5}, {'G', 6}, {'H', 7}};
     game.menu();
     
     while (true)
     {
         game.show_board();
         int old_row, new_row;
-        std::string old_column, new_column;
+        char old_column, new_column;
 
-        cout << "Choose the row of your figure: ";
-        cin >> old_row;
+        std::string old_coords, new_coords;
 
-        cout << "Choose the column of your figure: ";
-        cin >> old_column;
-
-        cout << "Choose the new row of your figure: ";
-        cin >> new_row;
-
-        cout << "Choose the new column of your figure: ";
-        cin >> new_column;
-
+        while (true)
+        {
+            cout << "Type the coordinates of your figure: ";
+            cin >> old_coords;
+            if (old_coords.size() != 2)
+                cout << "Wrong length of coordinates: try again!\n";
+            else if (!(std::isdigit(old_coords[0])))
+                cout << "First character is not a number: try again!\n";
+            else if (((int)(old_coords[0]) - '0') < 1 || ((int)(old_coords[0]) - '0') > 8)
+                cout << "First character is not a valid row number: try again!\n";
+            else if (m[old_coords[1]] == 0 && old_coords[1] != 'A')
+                cout << "Second character is not a valid column: try again!\n";
+            else
+                break;
+        }
+        old_row = (int)(old_coords[0]) - '0';
         old_row--;
-        new_row--;
+        old_column = old_coords[1];
 
-        if (game.get_current_player()->get_color() == 'w')
-            game.move_figure(std::make_tuple(7 - old_row, m[old_column]), std::make_tuple(7 - new_row, m[new_column]));
-        else
-            game.move_figure(std::make_tuple(7 - old_row, m[old_column]), std::make_tuple(7 - new_row, m[new_column]));
+        while (true)
+        {
+            cout << "Type the coordinates of your figure: ";
+            cin >> new_coords;
+            if (new_coords.size() != 2)
+                cout << "Wrong length of coordinates: try again!\n";
+            else if (!(std::isdigit(new_coords[0])))
+                cout << "First character is not a number: try again!\n";
+            else if (((int)(new_coords[0]) - '0') < 1 || ((int)(new_coords[0]) - '0') > 8)
+                cout << "First character is not a valid row number: try again!\n";
+            else if (m[new_coords[1]] == 0 && new_coords[1] != 'A')
+                cout << "Second character is not a valid column: try again!\n";
+            else
+                break;
+        }
+        new_row = (int)(new_coords[0]) - '0';
+        new_row--;
+        new_column = new_coords[1];
+
+        game.move_figure(std::make_tuple(7 - old_row, m[old_column]), std::make_tuple(7 - new_row, m[new_column]));
+        
         game.next_turn();
     }
     return 0;
