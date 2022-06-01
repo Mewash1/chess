af1299cf8676246f8928ab49ee84f9da9acc75f3
Milan <milanw.2001@gmail.com>
2022-06-01 08:52:15 +0200

    include horse quick fix

--- PlayerLib/include_all_figures.h
+++ PlayerLib/include_all_figures.h
@@ -1,6 +1,6 @@
 #include "../ChessLib/figures/king.h"
 #include "../ChessLib/figures/bishop.h"
 #include "../ChessLib/figures/pawn.h"
-#include "../ChessLib/figures/knight.h"
+#include "../ChessLib/figures/horse.h"
 #include "../ChessLib/figures/rook.h"
 #include "../ChessLib/figures/queen.h"
