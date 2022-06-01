e50383ebb482701507c4639847953dec067b6a3d
Milan <milanw.2001@gmail.com>
2022-06-01 11:23:09 +0200

    pvp fix return move description in move figure

--- GameLib/game.cpp
+++ GameLib/game.cpp
@@ -1,11 +1,11 @@
 #include "game.h"
 #include <iostream>
 #include <map>
-#include <stdlib.h>     /* srand, rand */
+#include <stdlib.h> /* srand, rand */
 #include <time.h>
 
-using std::cout;
 using std::cin;
+using std::cout;
 Game::Game()
 {
     this->player1 = NULL;
@@ -38,13 +38,18 @@ void Game::menu()
             break;
     }
 
-    Player* player1 = new Player(color, "Player 1");
+    Player *player1 = new Player(color, "Player 1");
     if (color == 'w')
         color = 'b';
     else
         color = 'w';
-    Player* player2 = new Player(color, "Player 2", false);
-    Board* board = new Board(player1, player2);
+
+    if (gamemode == 1)
+        Player *player2 = new Player(color, "Player 2");
+    else
+        Player *player2 = new Player(color, "Player 2", false);
+
+    Board *board = new Board(player1, player2);
 
     this->player1 = player1;
     this->player2 = player2;
@@ -55,76 +60,78 @@ void Game::menu()
     this->board = board;
 }
 
-void Game::player_turn()
+std::string Game::player_turn()
 {
-        this->show_board();
-        this->show_graveyard();
-        int old_row, new_row;
-        char old_column, new_column;
-        std::map<char, int> m = {{'a', 0}, {'b', 1}, {'c', 2}, {'d', 3}, {'e', 4}, {'f', 5}, {'g', 6}, {'h', 7}};
-        std::string old_coords, new_coords;
+
+    std::string temp_return; // used to return
+    this->show_board();
+    this->show_graveyard();
+    int old_row, new_row;
+    char old_column, new_column;
+    std::map<char, int> m = {{'a', 0}, {'b', 1}, {'c', 2}, {'d', 3}, {'e', 4}, {'f', 5}, {'g', 6}, {'h', 7}};
+    std::string old_coords, new_coords;
+    while (true)
+    {
         while (true)
         {
-            while (true)
-            {
-                cout << "Type the orgin of your figure: ";
-                cin >> old_coords;
-                if (old_coords.size() != 2)
-                    cout << "Wrong length of coordinates: try again!\n";
-                else if (!(std::isdigit(old_coords[0])))
-                    cout << "First character is not a number: try again!\n";
-                else if (((int)(old_coords[0]) - '0') < 1 || ((int)(old_coords[0]) - '0') > 8)
-                    cout << "First character is not a valid row number: try again!\n";
-                else if (m[tolower(old_coords[1])] == 0 && tolower(old_coords[1]) != 'a')
-                    cout << "Second character is not a valid column: try again!\n";
-                else
-                    break;
-            }
-            old_row = (int)(old_coords[0]) - '0';
-            old_row--;
-            old_column = old_coords[1];
-
-            while (true)
-            {
-                cout << "Type the target of your figure: ";
-                cin >> new_coords;
-                if (new_coords.size() != 2)
-                    cout << "Wrong length of coordinates: try again!\n";
-                else if (!(std::isdigit(new_coords[0])))
-                    cout << "First character is not a number: try again!\n";
-                else if (((int)(new_coords[0]) - '0') < 1 || ((int)(new_coords[0]) - '0') > 8)
-                    cout << "First character is not a valid row number: try again!\n";
-                else if (m[tolower(new_coords[1])] == 0 && tolower(new_coords[1]) != 'a')
-                    cout << "Second character is not a valid column: try again!\n";
-                else
-                    break;
-            }
-
-            new_row = (int)(new_coords[0]) - '0';
-            new_row--;
-            new_column = tolower(new_coords[1]);
-            try
-            {
-                this->move_figure(std::make_tuple(7 - old_row, m[old_column]), std::make_tuple(7 - new_row, m[new_column]));
-            }
-            catch (const std::exception &exc)
-            {
-                cout << exc.what() << endl;
-                continue;
-            }
-            catch (...)
-            {
-                cout << "unnkown error" << endl;
-                continue;
-            }
-            break;
+            cout << "Type the orgin of your figure: ";
+            cin >> old_coords;
+            if (old_coords.size() != 2)
+                cout << "Wrong length of coordinates: try again!\n";
+            else if (!(std::isdigit(old_coords[0])))
+                cout << "First character is not a number: try again!\n";
+            else if (((int)(old_coords[0]) - '0') < 1 || ((int)(old_coords[0]) - '0') > 8)
+                cout << "First character is not a valid row number: try again!\n";
+            else if (m[tolower(old_coords[1])] == 0 && tolower(old_coords[1]) != 'a')
+                cout << "Second character is not a valid column: try again!\n";
+            else
+                break;
         }
-}
+        old_row = (int)(old_coords[0]) - '0';
+        old_row--;
+        old_column = old_coords[1];
 
+        while (true)
+        {
+            cout << "Type the target of your figure: ";
+            cin >> new_coords;
+            if (new_coords.size() != 2)
+                cout << "Wrong length of coordinates: try again!\n";
+            else if (!(std::isdigit(new_coords[0])))
+                cout << "First character is not a number: try again!\n";
+            else if (((int)(new_coords[0]) - '0') < 1 || ((int)(new_coords[0]) - '0') > 8)
+                cout << "First character is not a valid row number: try again!\n";
+            else if (m[tolower(new_coords[1])] == 0 && tolower(new_coords[1]) != 'a')
+                cout << "Second character is not a valid column: try again!\n";
+            else
+                break;
+        }
+
+        new_row = (int)(new_coords[0]) - '0';
+        new_row--;
+        new_column = tolower(new_coords[1]);
+        try
+        {
+            temp_return = (*this).move_figure(std::make_tuple(7 - old_row, m[old_column]), std::make_tuple(7 - new_row, m[new_column]));
+        }
+        catch (const std::exception &exc)
+        {
+            cout << exc.what() << endl;
+            continue;
+        }
+        catch (...)
+        {
+            cout << "unnkown error" << endl;
+            continue;
+        }
+        break;
+    }
+    return temp_return;
+}
 
 void Game::cpu_turn()
 {
-    srand (time(NULL));
+    srand(time(NULL));
     while (true)
     {
         int x = rand() % 8;
@@ -133,20 +140,20 @@ void Game::cpu_turn()
         int new_y = rand() % 8;
 
         try
-            {
-                this->move_figure(std::make_tuple(x, y), std::make_tuple(new_x, new_y));
-            }
-            catch (const std::exception &exc)
-            {
-                // cout << exc.what() << endl;
-                continue;
-            }
-            catch (...)
-            {
-                // cout << "unnkown error" << endl;
-                continue;
-            }
-            break;
+        {
+            this->move_figure(std::make_tuple(x, y), std::make_tuple(new_x, new_y));
+        }
+        catch (const std::exception &exc)
+        {
+            // cout << exc.what() << endl;
+            continue;
+        }
+        catch (...)
+        {
+            // cout << "unnkown error" << endl;
+            continue;
+        }
+        break;
     }
 }
 
--- GameLib/game.h
+++ GameLib/game.h
@@ -6,17 +6,18 @@ class Game
 {
 private:
     Player *player1, *player2, *current_player;
-    Board* board;
+    Board *board;
+
 public:
     Game();
     void menu();
-    void show_board() {board->print();}
-    void show_graveyard() {board->print_graveyard();}
-    void move_figure(std::tuple<int, int> old_cor, std::tuple<int, int> new_cor) {board->move_figure(old_cor, new_cor);}
-    void player_turn();
+    void show_board() { board->print(); }
+    void show_graveyard() { board->print_graveyard(); }
+    std::string move_figure(std::tuple<int, int> old_cor, std::tuple<int, int> new_cor) { return board->move_figure(old_cor, new_cor); }
+    std::string player_turn();
     void cpu_turn();
     void switch_player();
-    Player* get_player1() {return player1;}
-    Player* get_player2() {return player2;}
-    Player* get_current_player() {return current_player;}
+    Player *get_player1() { return player1; }
+    Player *get_player2() { return player2; }
+    Player *get_current_player() { return current_player; }
 };
